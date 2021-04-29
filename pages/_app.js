d61a12a82954cac08d7b1d00dc41cb7f2affdd60
anggitrestu <anggitrestu60@gmail.com>
2021-04-29 20:57:26 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import '../tailwindcss/style.css';
+// * custom app, apapun halaman yang ada akan di tangkap disini dulu, baru di render setalahnya
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
